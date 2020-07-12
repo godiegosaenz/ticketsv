7473511ba2dedc636545f3481d7cb80aba4ea672
Diego Bermudez <godiegosaenz@gmail.com>
2020-07-12 13:34:28 -0500

    Inicio de aplicacion

---
+++ resources/lang/en/panel.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'site_title' => 'Sistema de atención Ciudadana',
+];
---
+++ resources/views/tickets/create.blade.php
@@ -0,0 +1,189 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            @if(session('status'))
+                <div class="alert alert-success" role="alert">
+                    {!! session('status') !!}
+                </div>
+            @endif
+            <div class="card">
+                <div class="card-header">Crear ticket</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('tickets.store') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="author_name" class="col-md-4 col-form-label text-md-right">Tu Cédula</label>
+
+                            <div class="col-md-6">
+                                <input id="author_dni" type="text" class="form-control @error('author_dni') is-invalid @enderror" name="author_dni" value="{{ old('author_dni') }}" required autocomplete="Cedula" autofocus>
+
+                                @error('author_dni')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="author_name" class="col-md-4 col-form-label text-md-right">Tu Nombre</label>
+
+                            <div class="col-md-6">
+                                <input id="author_name" type="text" class="form-control @error('author_name') is-invalid @enderror" name="author_name" value="{{ old('author_name') }}" required autocomplete="name">
+
+                                @error('author_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="author_email" class="col-md-4 col-form-label text-md-right">Tu Correo</label>
+
+                            <div class="col-md-6">
+                                <input id="author_email" type="email" class="form-control @error('author_email') is-invalid @enderror" name="author_email" value="{{ old('author_email') }}" required autocomplete="email">
+
+                                @error('author_email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row {{ $errors->has('category_id') ? 'has-error' : '' }}">
+                            <label for="category" class="col-md-4 col-form-label text-md-right">{{ trans('cruds.ticket.fields.category') }}</label>
+                            <div class="col-md-6">
+                                <select name="category_id" id="category" class="form-control select2" required>
+                                    @foreach($categories as $id => $category)
+                                        <option value="{{ $id }}" {{ (isset($ticket) && $ticket->category ? $ticket->category->id : old('category_id')) == $id ? 'selected' : '' }}>{{ $category }}</option>
+                                    @endforeach
+                                </select>
+                                @if($errors->has('category_id'))
+                                    <em class="invalid-feedback">
+                                        {{ $errors->first('category_id') }}
+                                    </em>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">@lang('cruds.ticket.fields.title')</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title">
+
+                                @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="content" class="col-md-4 col-form-label text-md-right">@lang('cruds.ticket.fields.content')</label>
+
+                            <div class="col-md-6">
+                                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content" rows="3" required>{{ old('content') }}</textarea>
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="attachments" class="col-md-4 col-form-label text-md-right">{{ trans('cruds.ticket.fields.attachments') }}</label>
+
+                            <div class="col-md-6">
+                                <div class="needsclick dropzone @error('attachments') is-invalid @enderror" id="attachments-dropzone">
+                
+                                </div>
+                            </div>
+                            @error('attachments')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    @lang('global.submit')
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+    var uploadedAttachmentsMap = {}
+Dropzone.options.attachmentsDropzone = {
+    url: '{{ route('tickets.storeMedia') }}',
+    maxFilesize: 2, // MB
+    addRemoveLinks: true,
+    headers: {
+      'X-CSRF-TOKEN': "{{ csrf_token() }}"
+    },
+    params: {
+      size: 2
+    },
+    success: function (file, response) {
+      $('form').append('<input type="hidden" name="attachments[]" value="' + response.name + '">')
+      uploadedAttachmentsMap[file.name] = response.name
+    },
+    removedfile: function (file) {
+      file.previewElement.remove()
+      var name = ''
+      if (typeof file.file_name !== 'undefined') {
+        name = file.file_name
+      } else {
+        name = uploadedAttachmentsMap[file.name]
+      }
+      $('form').find('input[name="attachments[]"][value="' + name + '"]').remove()
+    },
+    init: function () {
+@if(isset($ticket) && $ticket->attachments)
+          var files =
+            {!! json_encode($ticket->attachments) !!}
+              for (var i in files) {
+              var file = files[i]
+              this.options.addedfile.call(this, file)
+              file.previewElement.classList.add('dz-complete')
+              $('form').append('<input type="hidden" name="attachments[]" value="' + file.file_name + '">')
+            }
+@endif
+    },
+     error: function (file, response) {
+         if ($.type(response) === 'string') {
+             var message = response //dropzone sends it's own error messages in string
+         } else {
+             var message = response.errors.file
+         }
+         file.previewElement.classList.add('dz-error')
+         _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+         _results = []
+         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+             node = _ref[_i]
+             _results.push(node.textContent = message)
+         }
+
+         return _results
+     }
+}
+</script>
+@stop
